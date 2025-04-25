c383c347c59d4b9e45204e191d6bb37d1273c7f2
Guilherme Silva <gui.ghs.silva@gmail.com>
2025-04-25 20:00:08 -0300

    Inicio no arquivo JS, pegando os elementos do DOM e criando um array para o armazenamento de todas as tarefas

---
+++ main.js
@@ -0,0 +1,7 @@
+// Pegando os elementos do DOM
+const form = document.getElementById('formulario-tarefa');
+const tarefa = document.getElementById('tarefa');
+const lista = document.getElementById('lista-tarefas');
+
+// Criando um array para armazenar as tarefas
+let tarefas = [];
